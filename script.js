2b856d881a2f10983af755bdea02c0cbc6050aa2
jason parser <cucya100@gmail.com>
2021-03-24 16:48:54 +0300

    fix file btn and footer link btn

--- script.js
+++ script.js
@@ -3,13 +3,13 @@
 const overlay = document.getElementById("overlay")
 const overlayBlocker = document.getElementById("overlay-blocker")
 const bodyWrapper = document.getElementById("body-wrapper")
-const link = document.getElementById("modal-btn")
+const links = document.querySelectorAll(".modal-btn")
 const popup = document.getElementById("modal-content")
 const close = document.getElementById("modal-content-close")
 
-const realFileBtn = document.getElementById("real-file");
-const customBtn = document.getElementById("custom-button");
-const customTxt = document.getElementById("custom-text");
+const realFileBtn = document.getElementById("real-file")
+const customBtn = document.getElementById("custom-button")
+const customTxt = document.getElementById("custom-text")
 
 let isModalOpen = false
 
@@ -27,9 +27,11 @@ const openModal = () => {
   overlay.classList.add("overlay--open")
 }
 
-link.addEventListener("click", function (event) {
-  event.preventDefault()
-  openModal()
+links.forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault()
+    openModal()
+  })
 })
 
 close.addEventListener("click", function (event) {
@@ -58,25 +60,31 @@ const valuesBlockDots = document.getElementById("about-values-dots")
 
 let activeBlock = 0
 
-const valuesContent = [{
+const valuesContent = [
+  {
     color: "#2DA771",
-    description: "<b>СДЭК – это МЫ.</b> Мы любим свою работу и делаем ее хорошо. Мы помогаем и поддерживаем друг друга. От каждого из нас зависит результат.",
+    description:
+      "<b>СДЭК – это МЫ.</b> Мы любим свою работу и делаем ее хорошо. Мы помогаем и поддерживаем друг друга. От каждого из нас зависит результат.",
   },
   {
     color: "#ECC54A",
-    description: "<b>За посылкой – человек.</b> В каждом отправлении судьба, история, значимое событие для компании, человека и его близких. Именно это определяет все наши действия.",
+    description:
+      "<b>За посылкой – человек.</b> В каждом отправлении судьба, история, значимое событие для компании, человека и его близких. Именно это определяет все наши действия.",
   },
   {
     color: "#E36373",
-    description: "<b>Держим слово.</b> Чтобы держать своё слово, мы готовы делать больше, находить нестандартные решения для клиентов коллег и партнеров.",
+    description:
+      "<b>Держим слово.</b> Чтобы держать своё слово, мы готовы делать больше, находить нестандартные решения для клиентов коллег и партнеров.",
   },
   {
     color: "#B086B8",
-    description: "<b>Открытость.</b> Мы открыто делимся нашими достижениями и честно говорим об ошибках. Мы искренни с нашими клиентами и коллегами.",
+    description:
+      "<b>Открытость.</b> Мы открыто делимся нашими достижениями и честно говорим об ошибках. Мы искренни с нашими клиентами и коллегами.",
   },
   {
     color: "#5F95C8",
-    description: "<b>Становимся лучше.</b> Каждый день мы получаем новые знания, находим новые идеи, возможности и решения. Внедряем их в жизнь.",
+    description:
+      "<b>Становимся лучше.</b> Каждый день мы получаем новые знания, находим новые идеи, возможности и решения. Внедряем их в жизнь.",
   },
 ]
 
@@ -102,13 +110,14 @@ valuesBlockDots.querySelectorAll("li").forEach((el, index) => {
   })
 })
 
+// File Button Handler
 
-realFileBtn.addEventListener("change", function () {
-  if (realFileBtn.value) {
-    customTxt.innerHTML = realFileBtn.value.match(
-      /[\/\\]([\w\d\s\.\-\(\)]+)$/
-    )[1];
+realFileBtn.addEventListener("change", (event) => {
+  if (event.target.files.length > 0) {
+    const file = event.target.files[0]
+
+    customTxt.innerHTML = file.name
   } else {
-    customTxt.innerHTML = "Реквизиты";
+    customTxt.innerHTML = "Реквизиты"
   }
-});
+})
