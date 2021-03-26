988a8882cd133a15e749a2b7b576f2aa344a1d5f
jason parser <cucya100@gmail.com>
2021-03-26 17:53:10 +0300

    split scss files

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,14 @@
 // Import CSS files
 import "../css/style.scss"
+import "../css/about.scss"
+import "../css/advantages.scss"
+import "../css/contacts.scss"
+import "../css/cooperation.scss"
+import "../css/fulfillment.scss"
+import "../css/our-clients.scss"
+import "../css/reviews.scss"
+import "../css/modal.scss"
+import "../css/footer.scss"
 
 // Import JS files
 import "./modal"
