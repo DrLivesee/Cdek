08e8e6fb234861d252814f35a236ce18886ef25b
jason parser <cucya100@gmail.com>
2021-04-01 15:15:34 +0300

    wip: mobile burger menu

--- src/js/main-nav-mobile.js
+++ src/js/main-nav-mobile.js
@@ -1,11 +1,17 @@
-const clickOpen = document.getElementById("main-nav__mobile-open");
-const mainNav = document.getElementById("main-nav__mobile");
-const heading = document.getElementById("main-contract-heading");
-const modalBtn = document.getElementById("main-contract-btn");
+const clickOpen = document.getElementById("main-nav__mobile-open")
+const mainNav = document.getElementById("main-nav__mobile")
+const heading = document.getElementById("main-contract-heading")
+const modalBtn = document.getElementById("main-contract-btn")
+const mobileNavLinks = document.querySelectorAll(".main-nav__mobile a")
 
-let isMobileMenuOpen = false;
+const toggleMenu = () => {
+  mainNav.classList.toggle("main-nav__mobile--open")
+  heading.classList.toggle("heading-opacity")
+  modalBtn.classList.toggle("btn-opacity")
+}
 
-clickOpen.addEventListener("click", function () {
-    mainNav.classList.toggle("main-nav__mobile--open");
-    isMobileMenuOpen = true;
-});
+clickOpen.addEventListener("click", toggleMenu)
+
+mobileNavLinks.forEach((link) => {
+  link.addEventListener("click", toggleMenu)
+})
